6aec205bc06caafb7d9d8d94674845313f2ed821
tom <3250971246@qq.com>
2024-09-22 18:37:50 +0800

    更新material

--- apps/ansys_material_db/models.py
+++ apps/ansys_material_db/models.py
@@ -1,3 +1,17 @@
 import enum
 
 from django.db import models
+
+
+class MaterialModel(models.Model):
+    material_name = models.CharField(max_length=50, verbose_name="材料名")
+    electronics_material_property_id = models.OneToOneField(to="ElectronicsMaterialProperty", null=False, blank=False, on_delete=models.CASCADE)
+    workbench_material_property_id = ...
+
+
+class ElectronicsMaterialProperty(models.Model):
+    pass
+
+
+class WorkbenchMaterialProperty(models.Model):
+    pass
